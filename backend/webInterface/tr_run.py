4223d6ee433051114ae00b7d9cc90bd335d6f152
houance <45840516+houance@users.noreply.github.com>
2022-04-27 21:25:26 +0800

    Update tr_run.py

--- backend/webInterface/tr_run.py
+++ backend/webInterface/tr_run.py
@@ -120,7 +120,8 @@ class TrRun(tornado.web.RequestHandler):
                 tmp.append({'value': data[1], 'confidence': data[2]})
 
         response_data = {'code': 200, 'msg': '成功',
-                         'data': tmp}
+                         'data': {'raw_out': tmp,
+                                  'speed_time': round(time.time() - start_time, 2)}}
 
         if is_draw != '0':
             img_detected = img.copy()
